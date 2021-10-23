627bb4c30c863c96c94cdf41a77abf0e1c6879ee
lukistriyaf <flukis04>
2021-10-23 22:55:07 +0700

    Tailwind settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./node_modules/megadraft/dist/**/*.css",
+      "./components/**/*.js",
+      "./pages/**/*.js",
+    ],
+
+    defaultExtractor: (content) => {
+      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
   },
-}
+];
+
+module.exports = {
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
